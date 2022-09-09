3eafde9092623fc193f5ea9a14a65354d97250c8
Олег Терновий <olegternovij@MacBook-Air-Oleg.local>
2022-09-09 21:26:00 +0300

    third doploy

--- index.js
+++ index.js
@@ -14,37 +14,37 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('https://doctor-study-project.herokuapp.com//vaccines',VaccinesController.getVaccines)
+app.get('/vaccines',VaccinesController.getVaccines)
 
-app.post('https://doctor-study-project.herokuapp.com//vaccines',VaccinesController.addVaccine)
+app.post('/vaccines',VaccinesController.addVaccine)
 
-app.delete('https://doctor-study-project.herokuapp.com//vaccines/:id',VaccinesController.removeVaccine)
+app.delete('/vaccines/:id',VaccinesController.removeVaccine)
 
-app.put('https://doctor-study-project.herokuapp.com//vaccines/:id',VaccinesController.editVaccine)
-
-
-app.get('https://doctor-study-project.herokuapp.com//prices',PricesController.getPrices)
-
-app.post('https://doctor-study-project.herokuapp.com//prices',PricesController.addPrice)
-
-app.delete('https://doctor-study-project.herokuapp.com//prices/:id', PricesController.removePrice)
-
-app.put('https://doctor-study-project.herokuapp.com//prices/:id',PricesController.editPrice)
+app.put('/vaccines/:id',VaccinesController.editVaccine)
 
 
-app.get('https://doctor-study-project.herokuapp.com//patients', PatientsController.getPatients)
+app.get('/prices',PricesController.getPrices)
 
-app.post('https://doctor-study-project.herokuapp.com//patients',PatientsController.addPatient)
+app.post('/prices',PricesController.addPrice)
+
+app.delete('/prices/:id', PricesController.removePrice)
+
+app.put('/prices/:id',PricesController.editPrice)
 
 
-app.post('https://doctor-study-project.herokuapp.com//auth/registration',[
+app.get('/patients', PatientsController.getPatients)
+
+app.post('/patients',PatientsController.addPatient)
+
+
+app.post('/auth/registration',[
 	check("username","Логін має бути не менше 3 символів").isLength({min:3,max:20}),
 	check("password","Пароль має бути не менше 4 символів").isLength({min:4,max:20})
 ]
 , AuthController.registration)
 
-app.post('https://doctor-study-project.herokuapp.com//auth/login',AuthController.login)
+app.post('/auth/login',AuthController.login)
 
 
-app.listen(PORT,() => {
+app.listen(process.env.PORT || 5005,() => {
 }) 
